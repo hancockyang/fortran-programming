84a3aa4ffa86519a6bf36fe1cb8ba3724a8195b2
Hankang Yang <hancock.yang@gmail.com>
2016-01-30 11:25:45 -0500

    use interchange_sort function and add comments

--- 13.coarray(parallel computing)/sorting/sorting.f90
+++ 13.coarray(parallel computing)/sorting/sorting.f90
@@ -18,34 +18,31 @@ program sorting
 
    ! Variables
    integer, parameter:: N = 50000
+   ! codimension means the array (a) can be accessed by the other images
    double precision, dimension(N), codimension[*]::a
    double precision, dimension(N):: b
-   integer:: start, stop1,counts_per_second
+   real:: start, finish
    
    ! generate random numbers on image1
    if (this_image() == 1) then
       call random_seed()
       call random_number(a)
       b = a
-      call system_clock(start, counts_per_second)
+      call cpu_time(start)
    endif
+   ! need sync all beforce coarary
    sync all
-   ! copy half to image2
-!   if (this_image() == 2) then
-!      a(N/4+1:N/2) = a(N/4+1:N/2)[1]
-!   endif
-!   if (this_image() == 3) then
-!      a(N/2+1:3*N/4) = a(N/2+1:3*N/4)[1]
-!   endif
-!   if (this_image() == 4) then
-!      a(3*N/4+1:) = a(3*N/4+1:)[1]
-!   endif
+   ! copy 1/3 to image2
    if (this_image() == 2) then
+      ! a(N/3+1:2*N/3) on image1 is copied to image2
       a(N/3+1:2*N/3) = a(N/3+1:2*N/3)[1]
    endif
+   ! copy 1/3 to image3
    if (this_image() == 3) then
+      ! a(2*N/3+1:) on image1 is copied to image3
       a(2*N/3+1:) = a(2*N/3+1:)[1]
    endif
+   ! need sync all after data transfer
    sync all
    
    ! sorting on image1 and image2
@@ -58,6 +55,7 @@ program sorting
          call interchange_sort(a(2*N/3+1:))
       
    end select
+    ! need sync all after sorting
    sync all
 
    ! copy data on image2 to image1 and merge
@@ -66,20 +64,23 @@ program sorting
       a(:2*N/3) = merge2(a(:N/3), a(N/3+1:2*N/3))
       a(2*N/3+1:) = a(2*N/3+1:)[3]
       a = merge2(a(:2*N/3), a(2*N/3+1:))
-      call system_clock(stop1)
-      print *, "For 3-image sort, time = ", (stop1-start)/counts_per_second, "second"
+      call cpu_time(finish)
+      print *, "For 3-image sort, time = ", finish-start, "second"
       print *, a(1), a(N), all(a(:N-1)<=a(2:))
       
    endif
    ! sort in 1 image
    if (this_image() == 1) then
-      call system_clock(start)
-      call interchange_sort(b(:N/2))
-      call interchange_sort(b(N/2+1:))
-      b = merge2(b(:N/2), b(N/2+1:))
-      call system_clock(stop1)
+      call cpu_time(start)
+!      call interchange_sort(b(:N/3))
+!      call interchange_sort(b(N/3+1:2*N/3))
+!      call interchange_sort(b(2*N/3:))
+!      b(:2*N/3) = merge2(b(:N/3), b(N/3+1:2*N/3))
+!      b = merge2(b(:2*N/3), b(2*N/3+1:))
+      call interchange_sort(b)
+      call cpu_time(finish)
       print *
-      print *, "For 1-image sort, time = ", (stop1-start)/counts_per_second, "second"
+      print *, "For 1-image sort, time = ", finish-start, "second"
       print *, b(1), b(N), all(b(:N-1)<=b(2:))
       
    endif
